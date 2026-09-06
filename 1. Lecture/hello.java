6ec8d74ac05afaee4cad02a4b169444b9fa7bae2
mohdmaazzaidi <mohdmaazzaidi@gmail.com>
2026-09-06 17:40:52 +0530

    Removed class files

--- 1. Lecture/hello.java
+++ 1. Lecture/hello.java
@@ -3,10 +3,15 @@ public class hello {
         int a = 23;
         float ab = 2.3f;
         char ch = 'A';
+<<<<<<< HEAD
         boolean bl = true;
         //This is our first program
         float sum = a + (ab);
         System.out.println(sum);
+=======
+        boolean bl = true;;
+        System.out.println(a + ab);
+>>>>>>> 98515bd (Removed class files)
         System.out.println("Hello!");
 
     }
---
+++ 5. Lecture - Array/arrays.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class arrays {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int[] ab = new int[5];
+        for(int i = 0; i<5; i++){
+            ab[i] = sc.nextInt();
+        }
+        for(int i = 0; i<5; i++){
+            System.out.println(ab[i]);
+        }
+    }
+}
