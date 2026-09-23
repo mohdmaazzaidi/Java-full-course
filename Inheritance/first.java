e505dcbe441ca44df5cc42673d78d5c918974d62
mohdmaazzaidi <mohdmaazzaidi@gmail.com>
2026-09-23 15:26:25 +0530

    Abstraction

--- Inheritance/first.java
+++ Inheritance/first.java
@@ -1,19 +1,21 @@
 public class first {
     public static void main(String[] args) {
        BMW c1 = new BMW();
-       c1.printCar();
+       c1.printAlto();
     }
 }
 
 class Car{
+    int a = 200;
     void printCar(){
         System.out.println("I can start");
     }
 }
 
 class Alto extends Car{
+    int a = 300;
     void printAlto(){
-        System.out.println("This is only car that you can afford");
+        System.out.println("This is only car that you can afford"+ super.a);
     }
 
 }
